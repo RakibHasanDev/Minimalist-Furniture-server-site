be1d0bbd947e83dd98a952012df7bbffe34ecb4d
MrMernDeveloper <rakibkst16@gmail.com>
2022-11-24 01:51:13 +0600

    connect mongodb successfully

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.h32cfqq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+console.log(uri)
+
+
+
+
+
+
+
+
 
 app.get('/', async (req, res) => {
     res.send('Minimal Furniture server is running');
