81e89d6908ab79d4eacb3b2082b005432368f131
MrMernDeveloper <rakibkst16@gmail.com>
2022-11-26 23:54:32 +0600

    addded admin role

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -69,6 +69,12 @@ async function run() {
             const user = await usersCollection.findOne(query);
             res.send({ isSeller: user?.role === 'Seller' });
         });
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'Admin' });
+        });
         app.get('/bookings',  async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
@@ -81,6 +87,7 @@ async function run() {
             const products = await productCollection.find(query).toArray();
             res.send(products);
         });
+    
         app.get('/products/:name', async (req, res) => {
             const category = req.params.name;
             const query = { categoryName: category }
@@ -115,7 +122,15 @@ async function run() {
             const result = await bookingCollection.insertOne(booking);
             res.send(result);
         });
-        
+
+        app.delete("/products/:id", async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        });
+     
     }
 
     finally {
